3f2dee31aa1d8f270899fe8b61166fed9452a3f8
Akash D <deshbhratarakash@gmail.com>
2024-05-28 15:54:58 +0530

    Done: added test for single number input.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe StringCalculator do
         expect(string_calculator.add('')).to eq 0
       end
     end
+
+    context 'when input string have only one number' do
+      it 'returns the given number' do
+        expect(string_calculator.add('1')).to eq 1
+      end
+    end
   end
 end
